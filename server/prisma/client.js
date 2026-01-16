925f648e8ed11ce1ec71928f31edee7c0e7c1601
rashmik-2006 <rashmikrashmik2@gmail.com>
2026-01-16 16:51:08 +0530

    Create client.js

---
+++ server/prisma/client.js
@@ -0,0 +1,13 @@
+const { PrismaClient } = require("@prisma/client");
+
+let prisma;
+
+if (!global.prisma) {
+  global.prisma = new PrismaClient({
+    log: ["error"],
+  });
+}
+
+prisma = global.prisma;
+
+module.exports = prisma;
